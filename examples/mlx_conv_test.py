08893f7b6a2161170ba7c660b3e135a3a4535650
AlexPetrusca <petruscalex@gmail.com>
2025-12-18 16:28:01 -0800

    ok

--- examples/mlx_conv_test.py
+++ examples/mlx_conv_test.py
@@ -36,24 +36,6 @@ class ConvBlock(nn.Module):
         x = self.pool(x)
         return x
 
-
-class ConvPoolBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, pool_size=2):
-        super().__init__()
-        self.conv = nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size
-        )
-        self.pool = nn.MaxPool2d(kernel_size=pool_size)
-
-    def __call__(self, x):
-        x = self.conv(x)
-        x = nn.leaky_relu(x)
-        x = self.pool(x)
-        return x
-
-
 # Visualize
 def viz_sample_predictions(network, test_data, label_map, rows=5, cols=5, figsize=(10, 10)):
     fig, axes = plt.subplots(rows, cols, figsize=figsize, num="Sample Predictions")
